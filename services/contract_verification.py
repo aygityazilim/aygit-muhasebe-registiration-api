5dd9a92c48e6e835895366bd6d38e42d33cb19ce
kemalkaancetindag <kaancetindag@icloud.com>
2026-03-17 12:57:56 +0300

    contract verification logic changed

--- services/contract_verification.py
+++ services/contract_verification.py
@@ -21,16 +21,17 @@ class ContractVerificationService:
         self.registiration_history_repository = RegistirationHistoryRepository(db=db)
         self.contract_verification_repository = ContractVerificationRepository(db=db)
 
-    async def send_code(self, registiration_number: str, contract_id: int) -> None:
+    async def send_code(self, registiration_number: str) -> None:
         try:
             registiration = self.registiration_repository.get_by_field("tracking_number", registiration_number)
-            contract = self.contract_verification_repository.get_by_id(contract_id)
-            if contract.registiration_id != registiration.id:
-                raise  HTTPException(status_code=StatusCodeEnum.UNAUTHORIZED.value, detail=ErrorMessageEnum.UNAUTHORIZED.value)            
-            verification_code = str(random.randint(100000, 999999))
-            self.contract_verification_repository.update(contract, {"verification_code": verification_code, "sent_date": datetime.now()})
-            self.registiration_history_repository.create({"note": f"{contract.type} Verifikasyonu İstendi", "registiration_id": registiration.id, "status": RegistirationStatusEnum.PENDING.value})
-            self.db.commit()            
+            contracts = self.contract_verification_repository.get_registiration_contracts(registiration.id)
+            for contract in contracts:
+                if contract.registiration_id != registiration.id:
+                    raise HTTPException(status_code=StatusCodeEnum.UNAUTHORIZED.value, detail=ErrorMessageEnum.UNAUTHORIZED.value)
+                verification_code = str(random.randint(100000, 999999))
+                self.contract_verification_repository.update(contract, {"verification_code": verification_code, "sent_date": datetime.now()})
+                self.registiration_history_repository.create({"note": f"{contract.type} Verifikasyonu İstendi", "registiration_id": registiration.id, "status": RegistirationStatusEnum.PENDING.value})
+            self.db.commit()
         except HTTPException:
             raise
         except Exception as e:
